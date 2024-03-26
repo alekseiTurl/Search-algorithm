8aeaea3ce8cea6e5ed6765adde31ba4b94f610e0
turlaykov_a <turlaykov_a@cadesign.com>
2024-03-26 21:56:26 +0300

    add files

---
+++ index.js
@@ -0,0 +1,19 @@
+function searchEl(arr, el) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === el) {
+            return i
+        }
+    }
+    return new Error('Element not found')
+}
+function binarySearch(arr, el) {
+    let left = -1
+    let right = arr.length
+    while (right - left > 1) {
+        const mid = Math.floor((left + right) / 2)
+        if (arr[mid] === el) return mid
+        if (arr[mid] > el) right = mid
+        else left = mid
+    }
+    return new Error('Element not found')
+}
